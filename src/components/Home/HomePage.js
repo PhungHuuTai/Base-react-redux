a64565315ac708ab963e980c51ee39246bd37c6e
PhungHuuTai <phunghuutai7@gmail.com>
2025-05-17 22:53:58 +0700

    Design new header

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -4,8 +4,19 @@ const HomePage = (props) => {
     return (
         <div className="homepage-container">
             <video autoPlay loop muted>
-                <source src={videoHomepage} type="video/mp4"/>
+                <source src={videoHomepage} type="video/mp4" />
             </video>
+            <div className="homepage-content">
+                <div className="title-1">
+                    Get to know your customers with forms worth filling out
+                </div>
+                <div className="title-2">
+                    Collect all the data you need to understand customers with forms designed to be refreshingly different.
+                </div>
+                <div className="title-3">
+                    <button>Get's started. It's free.</button>
+                </div>
+            </div>
         </div>
     )
 }
