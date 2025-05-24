ad99bc3d21868f532f4df1afb1cd68f26ac5965b
PhungHuuTai <phunghuutai7@gmail.com>
2025-05-24 21:44:23 +0700

    API Services - Customize Axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus, FcPrevious } from 'react-icons/fc';
 import { toast } from 'react-toastify';
+import { postCreateNewUser } from '../../../services/apiService';
 
 const ModalCreateUser = ({ show, setShow }) => {
 
@@ -52,22 +52,15 @@ const ModalCreateUser = ({ show, setShow }) => {
             return;
         }
         //call API
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
+        let data = await postCreateNewUser(email, password, username, role, image);
 
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-        
-        if (res.data && res.data.EC === 0) {
-            toast.success(res.data.EM);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
             handleClose();
         }
 
-        if (res.data && res.data.EC !== 0) {
-            toast.error(res.data.EM);
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
         }
     }
 
