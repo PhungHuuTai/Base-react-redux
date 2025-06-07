b821c4517e43e73aa1ecc287d636d08f9ff8d029
PhungHuuTai <phunghuutai7@gmail.com>
2025-06-07 22:24:29 +0700

    Design Detail Quiz Layout

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import _ from "lodash";
+import "./DetailQuiz.scss";
 
 const DetailQuiz = () => {
     const params = useParams();
+    const location = useLocation();
     const quizId = params.id;
 
     useEffect(() => {
@@ -37,7 +39,30 @@ const DetailQuiz = () => {
 
     return (
         <div className="detail-quiz-container">
-            DetailQuiz
+            <div className="left-content">
+                <div className="q-title">
+                    Quiz {quizId}: {location?.state?.quizTitle}
+                </div>
+                <hr />
+                <div className="q-body">
+                    <img />
+                </div>
+                <div className="q-content">
+                    <div className="question">Question 1: How do you feel?</div>
+                    <div className="answer">
+                        <div className="a-child">A.abc</div>
+                        <div className="a-child">B.abc</div>
+                        <div className="a-child">C.abc</div>
+                    </div>
+                </div>
+                <div className="q-footer">
+                    <button className="btn btn-secondary">Prev</button>
+                    <button className="btn btn-primary">Next</button>
+                </div>
+            </div>
+            <div className="right-content">
+                Count Down
+            </div>
         </div>
     )
 }
