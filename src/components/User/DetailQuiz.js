46f6d4cbcc079fae85ca7f94d35c845a427a9148
PhungHuuTai <phunghuutai7@gmail.com>
2025-06-07 17:48:01 +0700

    Process Data on the frontend size

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,7 +1,7 @@
-import axios from "axios";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
 
 const DetailQuiz = () => {
     const params = useParams();
@@ -13,7 +13,26 @@ const DetailQuiz = () => {
 
     const fetchQuestions = async () => {
         const res = await getDataQuiz(quizId);
-        
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers)
+                    });
+
+                    return { questionId: key, questionDescription, image, answers }
+                })
+                .value();
+        }
     }
 
     return (
