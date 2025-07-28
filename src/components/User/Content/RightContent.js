0fe302d206f4db981c277b51de9476c254aa4696
PhungHuuTai <phunghuutai7@gmail.com>
2025-07-28 20:53:16 +0700

    Select question in the right-content

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,9 +1,42 @@
+import { useRef } from "react";
 import CountDown from "./CountDown";
 
-const RightContent = ({ dataQuiz, handleFinish }) => {
+const RightContent = ({ dataQuiz, handleFinish, setCurrQ }) => {
+    const refDiv = useRef([]);
     const onTimeUp = () => {
         handleFinish()
     }
+
+    const getClassQuestion = (question) => {
+        // check answered 
+        if (question && question.answers.length > 0) {
+            let isAnswered = question.answers.find(a => a.isSelected === true);
+            if (isAnswered) {
+                return "question selected";
+            }
+        }
+        return "question"
+    }
+
+    const handleClickQuestion = (question, index) => {
+        setCurrQ(index);
+        if (refDiv.current) {
+            refDiv.current.forEach(item => {
+                if (item && item.className === "question clicked") {
+                    item.className = "question"
+                }
+            })
+        }
+
+        if (question && question.answers.length > 0) {
+            let isAnswered = question.answers.find(a => a.isSelected === true);
+            if (isAnswered) {
+                return;
+            }
+        }
+        refDiv.current[index].className = "question clicked";
+    }
+
     return (
         <>
             <div className="main-timer">
@@ -15,7 +48,13 @@ const RightContent = ({ dataQuiz, handleFinish }) => {
                 {dataQuiz && dataQuiz.length > 0 &&
                     dataQuiz.map((item, index) => {
                         return (
-                            <div key={`question-${index}`} className="question">{index + 1}</div>
+                            <div
+                                key={`question-${index}`}
+                                className={getClassQuestion(item)}
+                                onClick={() => handleClickQuestion(item, index)}
+                                ref={element => refDiv.current[index] = element}
+                            >{index + 1}
+                            </div>
                         )
                     })
                 }
--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -156,6 +156,7 @@ const DetailQuiz = () => {
                 <RightContent 
                     dataQuiz={dataQuiz}
                     handleFinish={handleFinish}
+                    setCurrQ={setCurrQ}
                 />
             </div>
             <ModalResult
