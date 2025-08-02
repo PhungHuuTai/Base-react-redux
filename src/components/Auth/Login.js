273552d9b09eb93ac24b7f4ab0c74aa1bb4566a2
PhungHuuTai <phunghuutai7@gmail.com>
2025-08-02 16:11:56 +0700

    Design header multi languages

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
 import { doLogin } from '../../redux/action/userAction';
 import { ImSpinner9 } from 'react-icons/im';
+import Language from '../Header/Language';
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -62,6 +63,7 @@ const Login = () => {
             <div className='login-header'>
                 <span>You don't have an account yet?</span>
                 <button onClick={() => navigate('/register')}>Sign up</button>
+                <Language />
             </div>
             <div className='login-title col-4 mx-auto'>
                 PHT
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { logout } from '../../services/apiService';
 import { toast } from 'react-toastify';
 import { doLogout } from '../../redux/action/userAction';
+import Language from './Language';
 
 const Header = () => {
   const account = useSelector(state => state.user.account);
@@ -56,6 +57,7 @@ const Header = () => {
                 <NavDropdown.Item onClick={() => handleLogout()}>Log out</NavDropdown.Item>
               </NavDropdown>
             }
+            <Language />
           </Nav>
         </Navbar.Collapse>
       </Container>
